a5b2448bc082fded1604826dc791627a1dfb87b4
Oscar Mendoza <oscar.mendozaruiz@hotmail.com>
2021-11-09 20:02:01 -0500

    versionamiento y ajustesazure

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Azure  With Changes!!!');
+  res.send('Desplegando Contenedores con Exacom en AZURE!!!');
 });
 
 app.listen(config.port, () => {
